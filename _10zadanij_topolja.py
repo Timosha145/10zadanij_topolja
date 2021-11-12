f3ea8d45c5004c991108e0ec93789d38700d56d0
opilane <opilane@HP-CZC6297B9B>
2021-11-12 09:59:48 +0200

    aaa

--- _10zadanij_topolja.py
+++ _10zadanij_topolja.py
@@ -84,6 +84,65 @@
 #купи слона
 
 #a=input('Купи слона! ')
-while a.lower() !='слон':
-	a=input(f'все говорят {a}, а ты купи!')
-print('слон твой')
+#while a.lower() !='слон':
+#	a=input(f'все говорят {a}, а ты купи!')
+#print('слон твой')
+
+#задание 8
+
+#a=0
+#b=0
+#for d in range(20):
+#	a+=1
+#	b+=1
+#	a*=2.5
+#	print(f'{b} дьюм это {a}см')
+#	a/=2.5
+
+#задание 17
+
+#N=int(input('На какую цифру желаете таблицу умножения? - '))
+#for i in range(9):
+#	print(N,'*',i,'=',N*i)
+
+#задание 15
+
+#for b in range(1,10):
+#	for i in range(1,10):
+#		print(f'{(i*b):5}',end=' ')
+#	print()
+
+#задание 16
+
+#for b in range(1,10):	
+#	for i in range(1,10):
+#		if b==i:
+#			print((i), end=' ')
+#		else:
+#			print(0, end=' ')
+#	print()
+
+#задание 18
+
+#for i in range(20,50):
+#	if i%3==0 and i%5!=0:
+#		print(i)
+
+#задание 28
+
+#from random import *
+#a=randint(1,20)
+#n=0
+#U=int(input('Угадайка число от 1 до 20 - '))
+#if U==a:
+#	print(f'Угадал! не верных попыток было {n}')
+#else:
+#	n+=1
+#while U!=a:
+#	U=int(input('Не угадал, заново! - '))
+#	if U==a:
+#		print(f'Угадал! не верных попыток было {n}')
+#	else:
+#		n+=1
+
+#Задание
